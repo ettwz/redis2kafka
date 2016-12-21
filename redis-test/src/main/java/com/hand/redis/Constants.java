cf1942593ec9e55c29f7e01940f1df937fbea47b
ettwz <ettwz@hotmail.com>
2016-12-21 14:25:55 +0800

    81-80

--- redis-test/src/main/java/com/hand/redis/Constants.java
+++ redis-test/src/main/java/com/hand/redis/Constants.java
@@ -3,7 +3,7 @@ package com.hand.redis;
 public class Constants {
 
 	public static final String host = "172.20.1.19";
-	public static final int port = 6380;
+	public static final int port = 6381;
 	
 	public static final String SUBSCRIBE_CENTER = "_-subscribe-center-_";
 	
